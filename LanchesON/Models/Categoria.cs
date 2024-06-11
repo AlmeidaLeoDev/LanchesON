6acf3b3e05119e149a9a678d3021bbc279ceccf5
Leonaardo Almeida <almeidaleo.dev@gmail.com>
2024-06-11 00:14:27 -0300

    Modelo de domínio: criação de classes e relacionamento

---
+++ LanchesON/Models/Categoria.cs
@@ -0,0 +1,13 @@
+﻿namespace LanchesMac.Models
+{
+    public class Categoria
+    {
+        public int CategoriaId { get; set; }
+        public string CategoriaNome { get; set; }
+        public string Descricao { get; set; }
+
+        // Uma categoria possui muitos lanches
+        // Propriedade de navegação
+        public List<Lanche> Lanches { get; set; }
+    }
+}
---
+++ LanchesON/Models/Lanche.cs
@@ -0,0 +1,20 @@
+﻿namespace LanchesMac.Models
+{
+    public class Lanche
+    {
+        public int LancheId { get; set; }
+        public string Nome { get; set; }
+        public string DescricaoCurta { get; set; }
+        public string DescricaoDetalhada { get; set; }
+        public decimal Preco { get; set; }
+        public string ImagemUrl { get; set; }
+        public string ImagemThumbnailUrl { get; set; }
+        public bool IsLanchePreferido { get; set; }
+        public bool EmEstoque { get; set; }
+
+        // Chave estrangeira
+        public int CategoriaId { get; set; }
+        // Propriedade de navegação
+        public virtual Categoria Categoria { get; set; }
+    }
+}
