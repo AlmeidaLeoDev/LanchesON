26e9528a2c65d55b858b66ef0cfb50737d0a9892
Leonaardo Almeida <almeidaleo.dev@gmail.com>
2024-07-15 21:22:44 -0300

    Implementando o Registro

--- LanchesON/Startup.cs
+++ LanchesON/Startup.cs
@@ -31,6 +31,18 @@ public class Startup
             .AddEntityFrameworkStores<AppDbContext>()
             .AddDefaultTokenProviders();
 
+        // CASO VOCE QUEIRA SOBRESCREVER O PADRAO DO IDENTITY
+        //services.Configure<IdentityOptions>(options =>
+        //{
+        //    // Default Password settings.
+        //    options.Password.RequireDigit = false;
+        //    options.Password.RequireLowercase = false;
+        //    options.Password.RequireNonAlphanumeric = false;
+        //    options.Password.RequireUppercase = false;
+        //    options.Password.RequiredLength = 3;
+        //    options.Password.RequiredUniqueChars = 1;
+        //});
+
         /* O que faz: Diz ao contêiner de DI para criar novas instâncias de LancheRepository e CategoriaRepository sempre que ILancheRepository
         e ICategoriaRepository forem solicitados */
         services.AddTransient<ILancheRepository, LancheRepository>();
